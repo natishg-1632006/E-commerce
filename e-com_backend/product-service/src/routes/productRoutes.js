11e8695b83965e45638312012232171b7776761f
natishg-1632006 <natishg8@gmail.com>
2026-07-09 14:54:31 +0530

    Implement role-based access control middleware and update product routes to enforce authorization

--- e-com_backend/product-service/src/routes/productRoutes.js
+++ e-com_backend/product-service/src/routes/productRoutes.js
@@ -1,14 +1,55 @@
 const router = require('express').Router();
+
 const controller = require('../controllers/productController');
 const { createRules, updateRules } = require('../validations/productValidation');
+
 const authMiddleware = require('../middleware/authMiddleware');
+const authorize = require('../middleware/roleMiddleware');
 
+// Public Routes (No Login Required)
+router.get(
+  '/',
+  authMiddleware,
+  authorize('Admin', 'Customer'),
+  controller.getProducts
+);
 
-router.get('/search', controller.getProducts);
-router.get('/',authMiddleware,controller.getProducts);
-router.get('/:id', controller.getProduct);
-router.post('/', createRules, controller.createProduct);
-router.put('/:id', updateRules, controller.updateProduct);
-router.delete('/:id', controller.deleteProduct);
+router.get(
+  '/:id',
+  authMiddleware,
+  authorize('Admin', 'Customer'),
+  controller.getProduct
+);
+
+router.get(
+  '/search',
+  authMiddleware,
+  authorize('Admin', 'Customer'),
+  controller.getProducts
+);
+
+// Admin Only Routes
+router.post(
+  '/',
+  authMiddleware,
+  authorize('Admin'),
+  createRules,
+  controller.createProduct
+);
+
+router.put(
+  '/:id',
+  authMiddleware,
+  authorize('Admin'),
+  updateRules,
+  controller.updateProduct
+);
+
+router.delete(
+  '/:id',
+  authMiddleware,
+  authorize('Admin'),
+  controller.deleteProduct
+);
 
 module.exports = router;
